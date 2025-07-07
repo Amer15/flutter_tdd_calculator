21d8e17911cbc61583f85378524e8976a79c2159
Amer15 <amer95521@gmail.com>
2025-07-07 11:57:52 +0530

    feat: support for two comma-separated numbers

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -3,12 +3,16 @@ import 'package:tdd_calculator/calculator.dart';
 
 void main() {
   final calculator = StringCalculator();
-  
+
   test("returns 0 when empty string argument", () {
     expect(calculator.add(""), 0);
   });
 
   test("returns same number when only one argument is passed", () {
     expect(calculator.add("5"), 5);
+  });
+
+  test('returns sum of two comma-separated numbers', () {
+    expect(calculator.add("1,2"), 3);
   });
 }
